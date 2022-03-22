137771a3317f0399e6400a33eb1cb4df477e2faa
Danielle Fortin <fortind6@gmail.com>
2022-03-22 17:10:51 -0400

    added comment model

--- controllers/pizza-controller.js
+++ controllers/pizza-controller.js
@@ -7,6 +7,12 @@ const pizzaController = {
     getAllPizza(req, res) {
         //mongoose find method
         Pizza.find({})
+        .populate({
+            path: 'comments',
+            select: '-__v'
+        })
+        .select('--__v')
+        .sort({ _id: -1 })
         .then(dbPizzaData => res.json(dbPizzaData))
         .catch(err => {
             console.log(err);
@@ -17,6 +23,11 @@ const pizzaController = {
     //get one pizza by id
     getPizzaById({ params }, res) {
         Pizza.findOne({ _id: params.id })
+        .populate({
+            path: 'comments',
+            select: '-__v'
+        })
+        .select('-__v')
         .then(dbPizzaData => {
             // if no pizza is found, send 404
             if (!dbPizzaData) {
--- models/Pizza.js
+++ models/Pizza.js
@@ -1,24 +1,42 @@
 // import the dependencies
 const { Schema, model } = require('mongoose');
 
-const PizzaSchema = new Schema({
-    pizzaName: {
-        type: String,
-    },
-    createdBy: {
-        type: String,
-    },
-    createdAt: {
+const PizzaSchema = new Schema(
+    {
+      pizzaName: {
+        type: String
+      },
+      createdBy: {
+        type: String
+      },
+      createdAt: {
         type: Date,
-        default: Date.now
-    },
-    size: {
+        default: Date.now,
+      },
+      size: {
         type: String,
         default: 'Large'
+      },
+      toppings: [],
+      comments: [
+        {
+          type: Schema.Types.ObjectId,
+          ref: 'Comment'
+        }
+      ]
     },
-    toppings: []
-});
+    {
+      toJSON: {
+        virtuals: true,
+      },
+      id: false
+    }
+  );
 
+// get total count of comments and replies on retrieval
+PizzaSchema.virtual('commentCount').get(function() {
+    return this.comments.length;
+  });
 
 const Pizza = model('Pizza', PizzaSchema);
 
